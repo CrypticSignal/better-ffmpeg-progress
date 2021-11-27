72aed6fb3761b31cd1617a01d3c372efcd6da639
CrypticSignal <hshafiq@hotmail.co.uk>
2021-11-27 18:54:38 +0000

    Show a progress bar + other misc changes.

--- better_ffmpeg_progress/better_ffmpeg_progress.py
+++ better_ffmpeg_progress/better_ffmpeg_progress.py
@@ -1,9 +1,10 @@
-from argparse import ArgumentParser, RawTextHelpFormatter
 import os
 from pathlib import Path
 import subprocess
+import sys
 
 from ffmpeg import probe
+from tqdm import tqdm
 
 
 class FfmpegProcess:
@@ -13,7 +14,21 @@ class FfmpegProcess:
         Accepts an optional ffmpeg_loglevel parameter to set the value of FFmpeg's -loglevel argument.
         """
         self._command = command
-        self._ffmpeg_args = command + ["-progress", "pipe:1", "-nostats", "-loglevel", ffmpeg_loglevel]
+        index_of_filepath = self._command.index("-i") + 1
+        self._filepath = self._command[index_of_filepath]
+
+        self._can_get_duration = True
+
+        try:
+            self._duration_secs = float(probe(self._filepath)["format"]["duration"])
+        except Exception:
+            self._can_get_duration = False
+
+        self._ffmpeg_args = self._command + ["-loglevel", ffmpeg_loglevel]
+
+        if self._can_get_duration:
+            # pipe:1 sends the progress to stdout. See https://stackoverflow.com/a/54386052/13231825
+            self._ffmpeg_args += ["-progress", "pipe:1", "-nostats"]
 
     def run(self, progress_handler=None, ffmpeg_output_file=None):
         """
@@ -25,71 +40,62 @@ class FfmpegProcess:
         Progress: 25% | Speed: 22.3x | ETA: 1m 33s
         """
 
-        index_of_filepath = self._command.index("-i") + 1
-        filepath = self._command[index_of_filepath]
-
         if ffmpeg_output_file is None:
             os.makedirs("ffmpeg_output", exist_ok=True)
-            ffmpeg_output_file = os.path.join("ffmpeg_output", f"[{Path(filepath).name}].txt")
+            ffmpeg_output_file = os.path.join("ffmpeg_output", f"[{Path(self._filepath).name}].txt")
 
         with open(ffmpeg_output_file, "w") as f:
             pass
 
-        try:
-            file_duration = float(probe(filepath)["format"]["duration"])
-        except Exception:
-            can_get_duration = False
-            print(f"\nUnable to get the duration of {filepath}:\nThe improved progress stats will not be shown.")
+        print(f"Running: {' '.join(self._ffmpeg_args)}")
+        popen_args = [self._ffmpeg_args]
+
+        if self._can_get_duration:
+
+            with open(ffmpeg_output_file, "a") as f:
+                self._process = subprocess.Popen(
+                    self._ffmpeg_args, stdout=subprocess.PIPE, stderr=f
+                )
+
+            progress_bar = tqdm(total=self._duration_secs, unit="s", dynamic_ncols=True)
+            progress_bar.clear()
+            previous_seconds_processed = 0
         else:
-            can_get_duration = True
-
-        percentage = "unknown"
-        speed = "unknown"
-        eta_string = "unknown"
-        ffmpeg_stderr = ""
-
-        print("Running FFmpeg...")
-
-        with open(ffmpeg_output_file, "a") as f:
-            self._process = subprocess.Popen(
-                self._ffmpeg_args,
-                stdout=subprocess.PIPE,
-                stderr=f,
-            )
+            self._process = subprocess.Popen(self._ffmpeg_args)
 
         try:
-            while self._process.poll() is None:           
-                ffmpeg_output = self._process.stdout.readline().decode("utf-8").strip()
-
-                if ffmpeg_output is not None:
-                    if can_get_duration:
+            while self._process.poll() is None:
+                if self._can_get_duration:
+                    ffmpeg_output = self._process.stdout.readline().decode()
+                    # A progress handler was not specified. Use tqdm to show a progress bar.
+                    if progress_handler is None:
                         if "out_time_ms" in ffmpeg_output:
-                            microseconds = int(ffmpeg_output[12:])
-                            secs = microseconds / 1_000_000
-                            if file_duration is not None:
-                                percentage = round((secs / file_duration) * 100, 1)
-
-                        elif "speed" in ffmpeg_output:
-                            speed = ffmpeg_output.split("=")[1].strip()
-                            speed = 0 if " " in speed or "N/A" in speed else float(speed[:-1])
-                            if speed != 0:
-                                eta = (file_duration - secs) / speed
-                                minutes = int(eta / 60)
-                                seconds = round(eta % 60)
-                                eta_string = f"{minutes}m {seconds}s"
-
-                            if progress_handler is None:
-                                print(f"Progress: {percentage}% | Speed: {speed}x | ETA: {eta_string}", end="\r")
-                            else:
-                                progress_handler(percentage, f"{speed}x", eta)
-                    # ffprobe was unable to get the duration of the input file.
+                            seconds_processed = int(ffmpeg_output.strip()[12:]) / 1_000_000
+                            seconds_increase = seconds_processed - previous_seconds_processed
+                            progress_bar.update(seconds_increase)
+                            previous_seconds_processed = seconds_processed
+                    # A progress handler was specified.
                     else:
-                        print(self._process.stderr.readline().decode().strip())
+                        if "out_time_ms" in ffmpeg_output:
+                            seconds_processed = int(ffmpeg_output.strip()[12:]) / 1_000_000
+                            percentage = round((seconds_processed / self._duration_secs) * 100, 1)
 
-            width, height = os.get_terminal_size()
-            print("\r" + " " * (width - 1) + "\r", end="")
-            print("FFmpeg process complete.")
+                        if "speed" in ffmpeg_output:
+                            speed = ffmpeg_output.split("=")[1].strip()
+                            if " " in speed or "N/A" in speed:
+                                speed = None
+                            else:
+                                speed = float(speed[:-1])
+
+                            if speed:
+                                eta = (file_duration - secs) / speed
+                                progress_handler(percentage, f"{speed}x", eta)
+
+            progress_bar.close()
+            print(f"Done! Check out /{ffmpeg_output_file} to see the FFmpeg output.")
 
         except KeyboardInterrupt:
+            progress_bar.close()
             self._process.kill()
-            print("\nFFmpeg process killed.")
+            print("[KeyboardInterrupt] FFmpeg process killed. Exiting Better FFmpeg Progress.")
+            sys.exit(0)
